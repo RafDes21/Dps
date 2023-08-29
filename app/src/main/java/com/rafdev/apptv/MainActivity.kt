f6bf5a3971f3af12b5033a70ea4cdb776c87b652
RafDes21 <israeltineo84@gmail.com>
2023-08-29 17:05:59 -0300

    implente dependenci in build.gradle exoplayer2

--- app/src/main/java/com/rafdev/apptv/MainActivity.kt
+++ app/src/main/java/com/rafdev/apptv/MainActivity.kt
@@ -1,14 +1,46 @@
 package com.rafdev.apptv
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
 import com.rafdev.apptv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    //exoplayer
+    private lateinit var player: SimpleExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //create el objet exoplayer
+        player = SimpleExoPlayer.Builder(this).build()
+        // Conectamos el reproductor al reproductor de la vista
+        binding.playerView.player = player
+
+       try {
+           // Creamos un MediaItem utilizando la URL de la fuente del video
+           val mediaItem = MediaItem.fromUri(Uri.parse("https://redirector.dps.live/hls/ecuavisa2/playlist.m3u8"))
+
+           // Añadimos el MediaItem al reproductor
+           player.setMediaItem(mediaItem)
+
+           // Preparamos y reproducimos el video
+           player.prepare()
+           player.play()
+       }catch (e:Exception){
+           e.printStackTrace()
+       }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Liberamos los recursos del reproductor cuando se destruye la actividad
+        player.release()
     }
 }
